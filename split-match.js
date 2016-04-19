b64ff3638440e3bc102ee9334aa172aae795dd99
Ubuntu <ubuntu@ip-172-31-45-147.us-west-2.compute.internal>
2016-04-19 06:20:51 +0000

    starting code

--- split-match.js
+++ split-match.js
@@ -1,1 +1,24 @@
 #!/usr/bin/env node
+
+//  Pattern Matching Code Module
+var Transform = require('stream').Transform;
+var util = require('util');
+var program = require('commander');
+var fs = require('fs');
+
+if(!transform) {
+    transform  = require('readable-stream/transform')
+}
+
+function PatternMatch(pattern) { 
+    transform.call(
+    	this, 
+    	{ 
+    		objectMode: true 
+    	});
+    this.pattern = pattern
+}
+
+util.inherits(PatternMatch, Transform);
+
+
