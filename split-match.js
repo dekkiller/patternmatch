a7a73f59e6edb640fd2159437b301ac2b31eb489
Ubuntu <ubuntu@ip-172-31-45-147.us-west-2.compute.internal>
2016-04-19 06:37:07 +0000

    testing w/ framework given and test patternmatch code

--- split-match.js
+++ split-match.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 
 if(!transform) {
     transform  = require('readable-stream/transform')
-}
+};
 
 function PatternMatch(pattern) { 
     transform.call(
@@ -17,8 +17,44 @@ function PatternMatch(pattern) {
     		objectMode: true 
     	});
     this.pattern = pattern
-}
+};
 
 util.inherits(PatternMatch, Transform);
 
+PatternMatch.prototype._transform = function(chunk, encoding, done) { 
+    var data = chunk.toString();
+    this.push( 'INPUT:' ); 
+    this.push( data ); 
+    var parse = data.split(this.pattern)
+    
+    this._lastLineData = parse.splice( parse.length-1, 1)[0] 
+    
+    this.push('OUTPUT:');
+    for(var i in parse) {
+		this.push(parse[i]) 
+    }
+      
+};
 
+
+
+PatternMatch.prototype._flush = function(flushCompleted) { 
+	// Referenced from Strongloop
+    if(this._lastLineData) this.push( this._lastLineData )
+    this._lastLineData = null
+    flushCompleted() 
+};
+
+// CL Args
+program
+    .option('-p, --pattern <pattern>', 'Input Patterns such as . ,') 
+    .parse(process.argv)
+
+// input stream
+var inputStream = fs.createReadStream('./input-sensor.txt'); 
+
+// Pattern Matching stream
+var patternStream = inputStream.pipe(new PatternMatch(program.pattern));
+
+patternStream.on( 'readable', function() { 
+});
